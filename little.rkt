1c3e3df4f1e51d3f31c932af23066bcfce9dafb0
Jeff Forcier <jeff@bitprophet.org>
2012-05-10 12:02:39 -0700

    Use more compact lambdaless func defines

--- little.rkt
+++ little.rkt
@@ -3,47 +3,50 @@
 (require readline/rep-start)
 
 ; atom?
-(define atom?
-  (lambda (a)
-    (not (list? a))))
+(define (atom? a)
+  (not (list? a)))
 
 ; remove member
-(define rember
-  (lambda (a lat)
-    (cond
-      ((null? lat) '())
-      ((eq? (car lat) a) (cdr lat))
-      (else (cons (car lat) (rember a (cdr lat)))))))
+(define (rember a lat)
+  (cond
+    ((null? lat) '())
+    ((eq? (car lat) a) (cdr lat))
+    (else (cons (car lat) (rember a (cdr lat))))))
 
 ; first item of each list
-(define firsts
-  (lambda (lists)
-    (cond
-      ((null? lists) '())
-      (else (cons
-              (car (car lists))
-              (firsts (cdr lists)))))))
+(define (firsts lists)
+  (cond
+    ((null? lists) '())
+    (else (cons
+            (car (car lists))
+            (firsts (cdr lists))))))
 
 ; insert atom to right of other atom
-(define insertR
-  (lambda (new existing lat)
-    (cond
-      ((null? lat) '())
-      (else
-        (cond
-          ((eq? (car lat) existing)
-           (cons existing (cons new (cdr lat))))
-          (else
-            (cons (car lat) (insertR new existing (cdr lat)))))))))
+(define (insertR new existing lat)
+  (cond
+    ((null? lat) '())
+    (else
+      (cond
+        ((eq? (car lat) existing)
+         (cons existing (cons new (cdr lat))))
+        (else
+          (cons (car lat) (insertR new existing (cdr lat))))))))
 
 ; insert atom to LEFT of other atom!
-(define insertL
-  (lambda (new existing lat)
-    (cond
-      ((null? lat) '())
-      (else
-        (cond
-          ((eq? (car lat) existing)
-           (cons new lat))
-          (else
-            (cons (car lat) (insertL new existing (cdr lat)))))))))
+(define (insertL new existing lat)
+  (cond
+    ((null? lat) '())
+    (else
+      (cond
+        ((eq? (car lat) existing)
+         (cons new lat))
+        (else
+          (cons (car lat) (insertL new existing (cdr lat))))))))
+
+; substitute one atom for 1st occurrence of another
+(define (subst new old lat)
+  (cond
+    ((null? lat) '())
+    (else (cond
+      ((eq? (car lat) old) (cons new (cdr lat)))
+      (else (cons (car lat) (subst new old (cdr lat))))))))
