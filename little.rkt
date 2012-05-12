e9a19a6a2b609110029098f491106c77b51191fd
Jeff Forcier <jeff@bitprophet.org>
2012-05-11 19:56:46 -0700

    Occur*

--- little.rkt
+++ little.rkt
@@ -234,3 +234,14 @@
         ((eq? (car l) existing)
          (cons existing (cons new (insertR* new existing (cdr l)))))
         (else (cons (car l) (insertR* new existing (cdr l))))))))
+
+; recursive occur
+(define (occur* a l)
+  (cond
+    ((null? l) 0)
+    ((atom? (car l))
+     (cond
+       ((eq? (car l) a) (add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+    (else
+      (add (occur* a (car l)) (occur* a (cdr l))))))
